38a2737592f872e6a009f6005961d958d26f809d
Clément Josso <clement.josso@epfl.ch>
2025-11-15 23:35:02 +0100

    change platform scaling
    Technically works but the immovable platforms have weird hitboxes. to be fixed later

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,6 +3,9 @@ extends Node
 const LEVELS : Array = ["res://Scenes/Levels/Main.tscn"]
 var current_level: int = 0
 
+func next_level_safe():
+	call_deferred("next_level")
+
 func next_level():
 	current_level = (current_level + 1) % LEVELS.size()
 	get_tree().change_scene_to_file(LEVELS[current_level])
